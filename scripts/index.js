6b8b3d9380fcd675fb6c6dd91bc1eb4956c41996
Galina Glavatskii <gglavatskaya@yandex.com>
2024-11-13 15:10:11 +0200

    v1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+for (let i = 0; i < initialCards.length; i++) {
+    const card = addCard(initialCards[i], deleteCard);
+    placesList.append(card);
+}
 
-// @todo: Функция создания карточки
+function addCard(arrayElement, deleteCardCallback) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: Функция удаления карточки
+    const cardImage = card.querySelector('.card__image');
+    cardImage.setAttribute('src', arrayElement.link);
+    cardImage.setAttribute('alt', arrayElement.name);
 
-// @todo: Вывести карточки на страницу
+    const cardTitle = card.querySelector('.card__title');
+    cardTitle.textContent = arrayElement.name;
+
+    const deleteButton = card.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => {
+        deleteCardCallback(card);
+    });
+
+    return card;
+}
+
+function deleteCard(card) {
+    card.remove();
+}
