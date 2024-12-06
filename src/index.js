87a74e95f91d9c7da90b60c59f2a9b6bc4b1d6be
Galina Glavatskii <gglavatskaya@yandex.com>
2024-12-06 18:30:21 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
 import './pages/index.css';
 import { initialCards } from './scripts/cards'
-import { addCard, deleteCard,likeCard, openModalImage } from './scripts/components/card'
+import { createCard, deleteCard, likeCard, openModalImage } from './scripts/components/card'
 import { openModal, closeModal } from './scripts/components/modal'
 
 const placesList = document.querySelector('.places__list');
-// создание и добавление карточек на страницу
-function renderCards(array) {
-    while (placesList.firstChild) {
-        placesList.firstChild.remove()
-    }
-
-    for (let i = 0; i < array.length; i++) {
-        const card = addCard(array[i], deleteCard, likeCard, openModalImage);
-        placesList.append(card);
-    }
-}
-
-renderCards(initialCards);
-
-
-
+renderCards(placesList, initialCards);
 
 const editProfileBtn = document.querySelector('.profile__edit-button');
 const editProfileModal = document.querySelector('.popup_type_edit');
@@ -28,12 +13,28 @@ editProfileBtn.addEventListener('click', () => {
     openModal(editProfileModal);
 });
 
+const editProfileForm = document.forms.edit_profile;
+editProfileForm.addEventListener('submit', editProfileFormSubmit);
+
+
 const addNewCardsBtn = document.querySelector('.profile__add-button');
 const newCardModal = document.querySelector('.popup_type_new-card');
-
 addNewCardsBtn.addEventListener('click', () => openModal(newCardModal));
 
-//добавление имени профиля в модалку
+const newPlaceForm = document.forms.new_place;
+newPlaceForm.addEventListener('submit', newPlaceFormSubmit);
+
+function renderCards(container, cards) {
+    while (container.firstChild) {
+        container.firstChild.remove()
+    }
+
+    for (let i = 0; i < cards.length; i++) {
+        const card = createCard(cards[i], deleteCard, likeCard, openModalImage);
+        container.append(card);
+    }
+}
+
 function fillEditProfileModal() {
     const nameProfile = document.querySelector('.profile__title');
     const inputNameProfile = document.querySelector('.popup__input_type_name');
@@ -44,10 +45,7 @@ function fillEditProfileModal() {
     inputdescriptionProfile.value = descriptionProfile.textContent;
 }
 
-
-const editProfileForm = document.forms.edit_profile;
-
-function handleFormSubmit(evt) {
+function editProfileFormSubmit(evt) {
     evt.preventDefault();
 
     const nameValue = editProfileForm.querySelector('.popup__input_type_name').value;
@@ -59,18 +57,14 @@ function handleFormSubmit(evt) {
     closeModal(editProfileModal);
 }
 
-editProfileForm.addEventListener('submit', handleFormSubmit);
-
-const newPlaceForm = document.forms.new_place;
-
-function FormSubmit(evt) {
+function newPlaceFormSubmit(evt) {
     evt.preventDefault();
 
-    const cardNameInput = newPlaceForm.querySelector('.popup__input_type_card-name');
+    const cardName = newPlaceForm.querySelector('.popup__input_type_card-name');
     const cardUrl = newPlaceForm.querySelector('.popup__input_type_url');
 
     const newCard = {
-        name: cardNameInput.value,
+        name: cardName.value,
         link: cardUrl.value
     };
 
@@ -80,11 +74,3 @@ function FormSubmit(evt) {
     closeModal(newCardModal);
     newPlaceForm.reset();
 }
-
-newPlaceForm.addEventListener('submit', FormSubmit);
-
-
-
-
-
-
--- src/scripts/components/card.js
+++ src/scripts/components/card.js
@@ -1,51 +1,47 @@
-export function addCard(cardInfo, deleteCardCallback, likeCardCallback, openImageCallback) {
+import { openModal } from "./modal";
+
+export function createCard(cardInfo, deleteCardCallback, likeCardCallback, openImageCallback) {
     const cardTemplate = document.querySelector('#card-template').content;
     const card = cardTemplate.querySelector('.card').cloneNode(true);
-  
+
     const cardImage = card.querySelector('.card__image');
     cardImage.setAttribute('src', cardInfo.link);
     cardImage.setAttribute('alt', cardInfo.name);
-  
+
     const cardTitle = card.querySelector('.card__title');
     cardTitle.textContent = cardInfo.name;
-  
+
     const imageModal = document.querySelector('.popup_type_image');
-    imageModal.classList.add('popup_is-animated');
-  
     cardImage.addEventListener('click', () => {
-      openImageCallback(imageModal, cardInfo);
+        openImageCallback(imageModal, cardInfo);
     })
-  
+
     const deleteButton = card.querySelector('.card__delete-button');
     deleteButton.addEventListener('click', () => {
-      deleteCardCallback(card);
+        deleteCardCallback(card);
     });
-  
+
     const likeBtn = card.querySelector('.card__like-button');
     likeBtn.addEventListener('click', () => {
-      likeCardCallback(likeBtn);
+        likeCardCallback(likeBtn);
     });
-  
+
     return card;
-  }
-  
-  export function deleteCard(cardtarget) {
+}
+
+export function deleteCard(cardtarget) {
     cardtarget.remove();
-  }
-  
-  
-  export function likeCard(cardLikeBtn) {
+}
+
+export function likeCard(cardLikeBtn) {
     cardLikeBtn.classList.toggle('card__like-button_is-active');
-  }
-  
-  
-  
-  export function openModalImage(modal, card) {
+}
+
+export function openModalImage(modal, card) {
     const image = modal.querySelector('.popup__image');
     image.setAttribute('src', card.link);
     image.setAttribute('alt', card.name);
     const imageDescription = modal.querySelector('.popup__caption');
     imageDescription.textContent = card.name;
     openModal(modal);
-  }
-  
+}
